d842a10b77ad8de171e1112e6719b0967fe53120
laloutr3 <laloutr3@hotmail.fr>
2014-11-13 15:27:19 +0100

    Merge avec les projets d'antoine

---
+++ app/views/project/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.master')
+
+@section('sidebar')
+    <ul class="nav nav-sidebar">
+        <li><a href="/">Accueil</a></li>
+        <li class="active"><a href="/project">Projets</a></li>
+    </ul>
+@stop
+
+@section('breadcrumb')
+    <ol class="breadcrumb">
+      <li><a href="/">Accueil</a></li>
+      <li class="active">Projet</li>
+      <li><a href="/userstory">User Story</a></li>
+    </ol>
+@stop
+
+@section('content')
+    <h1>Modification de "{{ $project->name}}"</h1>
+
+    {{ HTML::ul($errors->all()) }}
+
+    {{ Form::model($project, array('route' => array('project.update', $project->id), 'method' => 'PUT')) }}
+
+    <div class="form-group">
+                {{ Form::label('name', 'Nom du Projet') }}
+                {{ Form::text('name', Input::old('projectName'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+                {{ Form::label('description', 'Description') }}
+                {{ Form::text('description', Input::old('description'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+                {{ Form::label('start', 'Date de début') }}
+                {{ Form::text('start', Input::old('projectBirthDay'), array('class' => 'form-control')) }}
+        </div>
+
+        <div class="form-group">
+                {{ Form::label('end', 'Date de fin') }}
+                {{ Form::text('end', Input::old('projectEndDay'), array('class' => 'form-control')) }}
+        </div>
+
+
+
+    {{ Form::submit('Terminer les modifications', array('class' => 'btn btn-primary')) }}
+
+    {{ Form::close() }}
+
+@stop
